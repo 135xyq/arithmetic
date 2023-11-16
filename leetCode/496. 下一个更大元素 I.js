ff2f3a413a021ed2091bc265629f2634c71511fa
谢永强 <xyq135@gmail.com>
2023-11-16 11:01:51 +0800

    leetcode 496. 下一个更大元素 I

---
+++ leetCode/496. 下一个更大元素 I.js
@@ -0,0 +1,28 @@
+/**
+ * nums1 中数字 x 的 下一个更大元素 是指 x 在 nums2 中对应位置 右侧 的 第一个 比 x 大的元素。
+ *
+ * 给你两个 没有重复元素 的数组 nums1 和 nums2 ，下标从 0 开始计数，其中nums1 是 nums2 的子集。
+ *
+ * 对于每个 0 <= i < nums1.length ，找出满足 nums1[i] == nums2[j] 的下标 j ，并且在 nums2 确定 nums2[j] 的 下一个更大元素 。如果不存在下一个更大元素，那么本次查询的答案是 -1 。
+ *
+ * 返回一个长度为 nums1.length 的数组 ans 作为答案，满足 ans[i] 是如上所述的 下一个更大元素 。
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function (nums1, nums2) {
+  const result = []
+
+  for (let i = 0; i < nums1.length; i++) {
+    const index = nums2.indexOf(nums1[i])
+    let next = -1
+    for (let j = index + 1; j < nums2.length; j++) {
+      if (nums2[j] > nums1[i]) {
+        next = nums2[j]
+        break
+      }
+    }
+    result.push(next)
+  }
+  return result
+}
