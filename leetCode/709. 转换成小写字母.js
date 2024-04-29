126b65a0442899533c0f54665dc64ad776d236fb
谢永强 <xieyq26@chinaunicom.cn>
2024-04-29 16:55:48 +0800

    leetcode  709. 转换成小写字母

---
+++ leetCode/709. 转换成小写字母.js
@@ -0,0 +1,11 @@
+// 给你一个字符串 s ，将该字符串中的大写字母转换成相同的小写字母，返回新的字符串。
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function (s) {
+  return s.toLowerCase()
+};
+
+console.log(toLowerCase('ssHH'))
